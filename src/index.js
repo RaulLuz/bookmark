17703fd13d106b35ea1e2f5d80e56ab060e38ecf
RaulLuz <raulodl@gmail.com>
2021-05-20 19:44:56 -0300

    Mobile overflow corrected

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <>
-    <App />
+    <div className="site">
+      <App />
+    </div>
+
   </>,
   document.getElementById('root')
 );
