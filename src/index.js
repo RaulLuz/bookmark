ed4ac894368263e87d81fdc66d61c006ee830cbb
RaulLuz <raulodl@gmail.com>
2021-05-20 21:58:23 -0300

    asa

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <>
-    {/* <div className="site"> */}
     <App />
-    {/* </div> */}
   </>,
   document.getElementById('root')
 );
